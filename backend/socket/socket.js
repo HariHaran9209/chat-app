f82e4abeacc8eb78f1e665811fd87687ea4ae741
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-03-22 12:05:09 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -23,17 +23,21 @@ io.on("connection", (socket) => {
 	console.log("a user connected", socket.id);
 
 	const userId = socket.handshake.query.userId;
-	if (userId != "undefined") userSocketMap[userId] = socket.id;
+	if (userId && userId !== "undefined") userSocketMap[userId] = socket.id;
 
-	// io.emit() is used to send events to all the connected clients
 	io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-	// socket.on() is used to listen to the events. can be used both on client and server side
+	// ✅ Listen for messages and send them to all users
+	socket.on("sendMessage", (message) => {
+		io.emit("receiveMessage", message); // Broadcast message to all clients
+	});
+
 	socket.on("disconnect", () => {
 		console.log("user disconnected", socket.id);
 		delete userSocketMap[userId];
 		io.emit("getOnlineUsers", Object.keys(userSocketMap));
 	});
 });
+
 
 export { app, io, server };
