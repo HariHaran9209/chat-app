3f08d9afe72ffb3251c2fdb0c35a20d6b2115a1f
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-03-22 20:34:25 +0530

    Update useListenMessages.js

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,26 +1,23 @@
 import { useEffect } from "react";
+
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
-import useGetMessages from "./useGetMessages";
+
+import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMessages = () => {
 	const { socket } = useSocketContext();
-	const { messages, setMessages } = useGetMessages();
-	const { selectedConversation } = useConversation();
+	const { messages, setMessages } = useConversation();
 
 	useEffect(() => {
-		if (!socket) return;
-
-		// Listen for incoming messages from the server
-		socket.on("receiveMessage", (message) => {
-			if (message.senderId === selectedConversation?._id) {
-				// ✅ Append the new message to the state
-				setMessages((prev) => [...prev, message]);
-			}
+		socket?.on("newMessage", (newMessage) => {
+			newMessage.shouldShake = true;
+			const sound = new Audio(notificationSound);
+			sound.play();
+			setMessages([...messages, newMessage]);
 		});
 
-		return () => socket.off("receiveMessage"); // Cleanup listener on unmount
-	}, [socket, selectedConversation, setMessages]);
+		return () => socket?.off("newMessage");
+	}, [socket, setMessages, messages]);
 };
-
 export default useListenMessages;
